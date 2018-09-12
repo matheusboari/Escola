2fea951236da29045ccd127c3f622a918fac0c99
Fernando <fsclaro@gmail.com>
2018-09-12 19:30:23 -0300

    atualização do dia 12/09

--- app/pages/css.inc.php
+++ app/pages/css.inc.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Gamja+Flower|Lato" rel="stylesheet">
-<link href="app/css/escola.inc.css" rel="stylesheet">
+<link href="app/css/escola.css" rel="stylesheet">
