20e0ef3477c42792a0d91285a357c1944a5c2ad0
Fernando <fsclaro@gmail.com>
2018-09-12 22:38:33 -0300

    Atualização da aula de 12/09

--- app/pages/css.inc.php
+++ app/pages/css.inc.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Gamja+Flower|Lato" rel="stylesheet">
 <link href="app/css/escola.css" rel="stylesheet">
+<link href="vendor/fontawesome/css/all.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -6,22 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <?php
-        include_once("app/pages/css.inc.php");
-    ?>
+    <?php include_once("app/pages/css.inc.php"); ?>
 
     <title>Gestão de Escolas</title>
 </head>
 
-<body>
-    <h1>Gestão de Escolas</h1>
+<body class="back">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Gestão Escolar</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><i class="fa fa-sign-in-alt"></i> Login</a></li>
+                    <li><a href="#">Registrar-se</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
 
 
-    <?php
-        include_once("app/pages/javascript.inc.php");
-    ?>
-
+    <?php include_once("app/pages/javascript.inc.php"); ?>
 </body>
 
-</html>
+</i>
