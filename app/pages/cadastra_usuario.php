58343f31a5bff90bc6df040241f59ec39f5d3526
Fernando Claro <fernando@MacBook.local>
2018-10-03 21:14:59 -0300

    Aula 03/10/2018

--- app/pages/cadastra_usuario.php
+++ app/pages/cadastra_usuario.php
@@ -10,18 +10,20 @@ $email = trim($email);
 
 $config = parse_ini_file('../../config.ini');
 
-$db = new mysqli (
-	$config["DB_HOST"],
-	$config["DB_USER"],
-	$config["DB_PASSWORD"],
-	$config["DB_DATABASE"]
+$db = new mysqli(
+  	$config['DB_HOST'],
+	$config['DB_USERNAME'],
+    $config['DB_PASSWORD'],
+    $config['DB_DATABASE']
 );
 
 if ($db->connect_errno) {
-	echo "Erro de acesso ao MySQL.<br>";
-	echo "Mensagem: (" . $db->connect_errno . ") " . $db->connect_error;
-} else {
-	echo "Acessado com sucesso o MySQL.";
+    echo "Erro de acesso ao MySQL.<br>";
+    echo "Mensagem: (" . $db->connect_errno . ") " . $db->connect_error;
 }
 
-?>
+$sql = "insert into usuarios (nome_usuario, email, senha)
+	    values ('$nome_usuario', '$email', '$senha');";
+
+$db->query($sql);
+$db->close();
